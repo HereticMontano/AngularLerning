c814c2314eeeef6cee63e2c64fcbecc3b3740874
Manuel <Manuelelp@gmail.com>
2026-05-19 16:52:04 +0200

    more css modifications

--- angularapp1.client/src/app/aboutme/aboutme.component.ts
+++ angularapp1.client/src/app/aboutme/aboutme.component.ts
@@ -3,7 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-aboutme',
   templateUrl: './aboutme.component.html',
-  standalone: false
+  standalone: false,
+  styleUrl: "./aboutme.css"
 })
 export class AboutMeComponent {
 }
